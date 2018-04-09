f44cde37d6ba53d3d66eef92a535ea8129334bea
Toru Mori <mori.toru4@exc.epson.co.jp>
2018-04-09 11:40:44 +0900

    抽出した情報の出力先ディレクトリを指定するように変更

--- app/git-change-files.ts
+++ app/git-change-files.ts
@@ -6,36 +6,38 @@
  */
 import { ChangeFileResolver, ChangeFile } from './git/change-file-resolver';
 import * as fs from 'fs';
+import * as path from 'path';
 
-function outputFiles(files: ChangeFile[]): void {
+function outputFiles(files: ChangeFile[], dir: string): void {
     const data = files.map((file) => {
         return file.getName();
     }).reduce((prev, current) => {
         return `${prev}\n${current}`;
     });
-    fs.writeFileSync('./file.txt', data);
+    fs.writeFileSync(path.join(dir, './file.txt'), data);
 }
 
-function outputFileRevisions(files: ChangeFile[]): void {
+function outputFileRevisions(files: ChangeFile[], dir: string): void {
     const data = files.map((file) => {
         return `${file.getName()},${file.getRevision()}`;
     }).reduce((prev, current) => {
         return `${prev}\n${current}`;
     });
-    fs.writeFileSync('./file-revision.txt', data);
+    fs.writeFileSync(path.join(dir, './file-revision.txt'), data);
 }
 
-if (process.argv.length !== 5) {
+if (process.argv.length !== 6) {
     throw Error('Illegal arguments.');
 }
 
 const sourcePath = process.argv[2];
 const fromBranch = process.argv[3];
 const toBranch = process.argv[4];
+const destPath = process.argv[5];
 
 const resolver = new ChangeFileResolver(sourcePath, fromBranch, toBranch);
 resolver.resolve();
 
 const changeFiles = resolver.getFiles();
-outputFiles(changeFiles);
-outputFileRevisions(changeFiles);
+outputFiles(changeFiles, destPath);
+outputFileRevisions(changeFiles, destPath);
