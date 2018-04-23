40282cddf9620d1080f98711a5dac174dfd0be48
Toru Mori <mori.toru4@exc.epson.co.jp>
2018-04-23 14:52:41 +0900

    GitLabコメント時に空白行を無視するように修正

--- app/gitlab/repository/abstract-repository.ts
+++ app/gitlab/repository/abstract-repository.ts
@@ -1,16 +1,21 @@
 export abstract class AbstractRepository {
     protected handleError(response: any): string {
-        if (!response.options || !response.statusCode) {
+        try {
+            if (!response.options || !response.statusCode) {
+                return response;
+            }
+            let result: string = '';
+            result += '--- request ---\n';
+            result += JSON.stringify(response.options) + '\n';
+            result += '--- response ---\n';
+            result += JSON.stringify({
+                statusCode: response.statusCode,
+                message: JSON.stringify(JSON.parse(response.error)),
+            }) + '\n';
+            return result;
+        } catch (error) {
+            console.log(error);
             return response;
         }
-        let result: string = '';
-        result += '--- request ---\n';
-        result += JSON.stringify(response.options) + '\n';
-        result += '--- response ---\n';
-        result += JSON.stringify({
-            statusCode: response.statusCode,
-            message: JSON.stringify(JSON.parse(response.error)),
-        }) + '\n';
-        return result;
     }
 }
