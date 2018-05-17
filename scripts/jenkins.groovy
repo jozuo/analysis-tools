4c51090b20f99bac6bd45206d313fb2e65473721
Toru Mori <mori.toru4@exc.epson.co.jp>
2018-05-17 09:58:29 +0900

    DEBUG情報の出力を フラグセット時に限定

--- scripts/jenkins.groovy
+++ scripts/jenkins.groovy
@@ -46,7 +46,7 @@ def changeGitLabStatusToPending() {
 }
 
 def changeGitLabStatus(status, description=null, coverage=null) {
-    docker.image('seig/analysys-tool:latest').inside("${env.DOCKER_HOST_OPTION}") {
+    docker.image('seig/analysis-tool:latest').inside("${env.DOCKER_HOST_OPTION}") {
         sh """
             ${env.PROXY_SETTING}
             cd node-tool
@@ -56,7 +56,7 @@ def changeGitLabStatus(status, description=null, coverage=null) {
 }
 
 def commentToGitLab(commentFilePath) {
-    docker.image('seig/analysys-tool:latest').inside("${env.DOCKER_HOST_OPTION}") {
+    docker.image('seig/analysis-tool:latest').inside("${env.DOCKER_HOST_OPTION}") {
         sh """
             ${env.PROXY_SETTING}
             cd node-tool
@@ -66,7 +66,7 @@ def commentToGitLab(commentFilePath) {
 }
 
 def resolveDiffFiles() {
-    docker.image('seig/analysys-tool:latest').inside {
+    docker.image('seig/analysis-tool:latest').inside {
         sh """
             ${env.PROXY_SETTING}
             cd node-tool
@@ -76,15 +76,17 @@ def resolveDiffFiles() {
 }
 
 def debugEnvironment() {
-    echo "GITLAB_CREDENTIAL_TOOL: ${env.GITLAB_CREDENTIAL_TOOL}"
-    echo "GITLAB_TOKEN: ${env.GITLAB_TOKEN}"
-    echo "GITLAB_URL: ${env.GITLAB_URL}"
-    echo "GITLAB_PROJECT_ID: ${env.GITLAB_PROJECT_ID}"
-    echo "GITLAB_BRANCH: ${env.GITLAB_BRANCH}"
-    echo "COMMIT_HASH_BEGIN: ${env.COMMIT_HASH_BEGIN}"
-    echo "COMMIT_HASH_END: ${env.COMMIT_HASH_END}"
-    echo "BUILD_URL: ${env.BUILD_URL}"
     echo "DEBUG: ${env.DEBUG}"
+    if (env.DEBUG == 'true') {
+        echo "GITLAB_CREDENTIAL_TOOL: ${env.GITLAB_CREDENTIAL_TOOL}"
+        echo "GITLAB_TOKEN: ${env.GITLAB_TOKEN}"
+        echo "GITLAB_URL: ${env.GITLAB_URL}"
+        echo "GITLAB_PROJECT_ID: ${env.GITLAB_PROJECT_ID}"
+        echo "GITLAB_BRANCH: ${env.GITLAB_BRANCH}"
+        echo "COMMIT_HASH_BEGIN: ${env.COMMIT_HASH_BEGIN}"
+        echo "COMMIT_HASH_END: ${env.COMMIT_HASH_END}"
+        echo "BUILD_URL: ${env.BUILD_URL}"
+    }
 }
 
 def setupEnvironment() {
@@ -111,7 +113,7 @@ def cloneSource() {
 
 def initTool() {
     dir('node-tool') {
-        def image = docker.build('seig/analysys-tool:latest', "${env.DOCKER_BUILD_OPTION} .");
+        def image = docker.build('seig/analysis-tool:latest', "${env.DOCKER_BUILD_OPTION} .");
         image.inside {
             sh """
                 ${env.PROXY_SETTING}
